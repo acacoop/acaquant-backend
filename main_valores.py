cc516c0ef26033cc1ea68d55e91dcf151f4548f7
nicolas.mollo <mollonicolas95@gmail.com>
2026-03-19 10:46:04 -0300

    primer commit

--- main_valores.py
+++ main_valores.py
@@ -19,7 +19,7 @@ TICKERS = [
     "MERV - XMEV - TZXM6 - 24hs", "MERV - XMEV - S17A6 - 24hs",
     "MERV - XMEV - S30A6 - 24hs", "MERV - XMEV - S29Y6 - 24hs", "MERV - XMEV - T30J6 - 24hs",
     "MERV - XMEV - S15Y6 - 24hs","MERV - XMEV - TTJ26 - 24hs","MERV - XMEV - TTS26 - 24hs",
-    "MERV - XMEV - TTD26 - 24hs"
+    "MERV - XMEV - TTD26 - 24hs",
     "MERV - XMEV - S31L6 - 24hs", "MERV - XMEV - S31G6 - 24hs", "MERV - XMEV - S30O6 - 24hs",
     "MERV - XMEV - S30N6 - 24hs", "MERV - XMEV - T15E7 - 24hs", "MERV - XMEV - T30A7 - 24hs",
     "MERV - XMEV - T31Y7 - 24hs", "MERV - XMEV - T30J7 - 24hs", "MERV - XMEV - TY30P - 24hs",

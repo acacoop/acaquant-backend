1c0ea77540107637cc6ecbd2523ad1987702f68f
Nicolas Mollo <nicomollo@MacBook-Air-de-Nicolas.local>
2026-03-23 01:41:03 -0300

    refactor: eliminar start_all.sh y main_options.py (redundantes con servicios systemd)

--- start_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /root/TradingAV
-
-/root/TradingAV/venv/bin/python /root/TradingAV/main_valores.py &
-
-/root/TradingAV/venv/bin/streamlit run /root/TradingAV/streamlit_app.py --server.port 8501 --server.headless true &
-
-wait
